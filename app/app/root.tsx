f20dc54d8dd69d6c1a4f58192b765c3feec43973
iemong <kiyoshidainagon@gmail.com>
2022-02-13 11:09:37 +0900

    prettier, lintを追加

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -1,17 +1,17 @@
 import {
-    Link,
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  Link,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from 'remix'
+import type { MetaFunction } from 'remix'
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+  return { title: 'New Remix App' }
+}
 
 export default function App() {
   return (
@@ -26,9 +26,9 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
         <Link to="/posts">Posts</Link>
       </body>
     </html>
-  );
+  )
 }
